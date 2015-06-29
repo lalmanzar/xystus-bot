6d76f4f371c7a51ccda980b16a2872743cf6adf7
lalmanzar <ruisu15@gmail.com>
2015-06-28 23:26:44 -0400

    use long polling

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var request = require('request');
 var config = require('../config.js');
 var debug = require('debug')('indexRoute');
+var _ = require('lodash');
+
 var router = express.Router();
 var lastId = -1;
+var interval;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -20,66 +23,68 @@ router.get('/me', function (req, res, next) {
 });
 
 router.get('/updates', function (req, res, next) {
+  getUpdates(res);
+});
+
+var getUpdates = function (res) {
   var url = config.telegram.url() + 'getUpdates';
   debug('requesting url: ' + url);
-  request(url, function (error, response, body) {
-    res.send(body);
-  });
-});
+  request({
+    url: url, //URL to hit
+    qs: { offset: lastId + 1 }, //Query string data
+    method: 'GET', //Specify the method
+  }, function (error, response, body) {
+      if (res) {
+        res.send(body);
+      } else {
+        var info = JSON.parse(body);
+        _(info.result).forEach(function (update) {
+          if (update.update_id > lastId) {
+            lastId = update.update_id;
+            if (update.message.text === '\/boobs' || update.message.text.indexOf('\/boobs ') === 0) {
 
-router.get('/set_webhook', function (req, res, next) {
-  var url = config.telegram.url() + 'setWebhook';
-  var hookUrl = req.protocol + '://' + req.get('host') + '/webhook';
-  debug('requesting url: ' + url);
-
-  request(
-    {
-      url: url, //URL to hit
-      qs: { url: hookUrl }, //Query string data
-      method: 'GET', //Specify the method
-    },
-    function (error, response, body) {
-      res.send(body);
+            } else if (update.message.text === '\/butts' || update.message.text.indexOf('\/butts ') === 0) {
+              request('http://api.obutts.ru/noise/1', function (error, response, body) {
+                var url = config.telegram.url() + 'sendMessage';
+                if (!error && response.statusCode == 200) {
+                  var imageArray = JSON.parse(body);
+                  var image = imageArray.length > 0 ? 'http://media.oboobs.ru/' + imageArray[0].preview : 'Image error';
+                  request(
+                    {
+                      url: url, //URL to hit
+                      qs: {
+                        chat_id: update.message.chat.id,
+                        text: image
+                      }, //Query string data
+                      method: 'GET', //Specify the method
+                    });
+                } else {
+                  request(
+                    {
+                      url: url, //URL to hit
+                      qs: {
+                        chat_id: update.message.chat.id,
+                        text: 'Image Error'
+                      }, //Query string data
+                      method: 'GET', //Specify the method
+                    });
+                }
+              });
+            }
+          }
+        });
+      }
     });
+};
+
+router.get('/set_longPolling', function (req, res, next) {
+  interval = setInterval(getUpdates, 500);
 });
 
-router.post('/webhook', function (req, res, next) {
-  debug(req.body);
-  if (req.body.update_id > lastId) {
-    lastId = req.body.update_id;
-    if (req.body.message.text === '\/boobs' || req.body.message.text.indexOf('\/boobs ') === 0) {
-
-    } else if (req.body.message.text === '\/butts' || req.body.message.text.indexOf('\/butts ') === 0) {
-      request('http://api.obutts.ru/noise/1', function (error, response, body) {
-        var url = config.telegram.url() + 'sendMessage';        
-        if (!error && response.statusCode == 200) {
-          var imageArray = JSON.parse(body);
-          var image = imageArray.length > 0 ? 'http://media.oboobs.ru/' + imageArray[0].preview : 'Image error';
-          request(
-            {
-              url: url, //URL to hit
-              qs: {
-                chat_id: req.body.message.chat.id,
-                text: image
-              }, //Query string data
-              method: 'GET', //Specify the method
-            });
-        } else {
-          request(
-            {
-              url: url, //URL to hit
-              qs: {
-                chat_id: req.body.message.chat.id,
-                text: 'Image Error'
-              }, //Query string data
-              method: 'GET', //Specify the method
-            });
-        }
-      });
-    }
-
+router.get('/stop_longPolling', function (req, res, next) {
+  if (interval) {
+    clearInterval(interval);
   }
-  res.send('OK');
 });
 
 module.exports = router;
