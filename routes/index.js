6a00f8a7cccf6f7301ef250fc222d320aea7a505
lalmanzar <ruisu15@gmail.com>
2015-11-28 00:56:50 -0400

    debug

--- routes/index.js
+++ routes/index.js
@@ -33,10 +33,11 @@ router.get('/updates', function (req, res, next) {
 
 bot.on('message', function(msg){
   msg = msg.trim();
+  console.log(msg);
   if(msg.indexOf('@XystusBot', msg.length - 10) !== -1){
     msg = msg.slice(0, -10);
   }
-  
+  console.log(msg);
   _(pluginsModules).filter(function(plugin){
     return plugin.isSupported(msg);
   }).forEach(function(plugin){
