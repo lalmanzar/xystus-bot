2e344f57f334deaabbc22ecab9a8be613c433275
lalmanzar <ruisu15@gmail.com>
2016-01-19 21:33:27 -0400

    revert

--- app.js
+++ app.js
@@ -6,55 +6,51 @@ var TelegramBot = require('node-telegram-bot-api')
 var _ = require('lodash');
 var pluginsModules = require('require-all')(__dirname + '/modules');
 
-var fs = require('fs');
-fs.writeFile(config.telegram.options.webHook.cert, process.env.CERT);
-fs.writeFile(config.telegram.options.webHook.key, process.env.CERT_KEY);
-// var bot = new TelegramBot(config.telegram.secretToken, config.telegram.options);
-// bot.setWebHook(config.telegram.appUrl + '/' + config.telegram.secretToken, config.telegram.options.webHook.cert);
+var bot = new TelegramBot(config.telegram.secretToken, config.telegram.options);
+bot.setWebHook(config.telegram.appUrl + '/' + config.telegram.secretToken, config.telegram.options.webHook.cert);
+
+_(pluginsModules).forEach(function(plugin){
+    if(_.isArray(plugin.regex)){
+        _.forEach(plugin.regex, function(regex){
+            bot.onText(regex, function (msg, match) {
+                plugin.proccess(msg, bot)
+            });
+        });
+    }else if(_.isRegExp(plugin.regex)){
+        bot.onText(plugin.regex, function (msg, match) {
+            plugin.proccess(msg, bot)
+        });
+    }
+  }).value();
+
+var app = express();
+
+// uncomment after placing your favicon in /public
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
-// _(pluginsModules).forEach(function(plugin){
-//     if(_.isArray(plugin.regex)){
-//         _.forEach(plugin.regex, function(regex){
-//             bot.onText(regex, function (msg, match) {
-//                 plugin.proccess(msg, bot)
-//             });
-//         });
-//     }else if(_.isRegExp(plugin.regex)){
-//         bot.onText(plugin.regex, function (msg, match) {
-//             plugin.proccess(msg, bot)
-//         });
-//     }
-//   }).value();
+app.get('/', function (req, res, next) {
+  res.send('Xystus\' bot says: Hello!');
+});
 
-// var app = express();
+app.get('/me', function (req, res, next) {
+  bot.getMe().then(function (me) {
+    res.send('Hi my name is '+ me.first_name +'!');
+  }).catch(function() {});
+});
 
-// // uncomment after placing your favicon in /public
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-
-// app.get('/', function (req, res, next) {
-//   res.send('Xystus\' bot says: Hello!');
-// });
-
-// app.get('/me', function (req, res, next) {
-//   bot.getMe().then(function (me) {
-//     res.send('Hi my name is '+ me.first_name +'!');
-//   }).catch(function() {});
-// });
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//     res.send('Not Found');
-// });
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.send('Internal Server Error');
-// });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    res.send('Not Found');
+});
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.send('Internal Server Error');
+});
 
 
-// module.exports = app;
+module.exports = app;
