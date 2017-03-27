3fdc80ac4a17a421e8b58707f18bc8f862a11f56
Luis Almanzar <ruisu15@gmail.com>
2017-03-27 00:58:21 -0400

    Update support.js

--- modules/support.js
+++ modules/support.js
@@ -1,7 +1,7 @@
 /// <reference path="../typings/node/node.d.ts"/>
 (function () {
   module.exports = {
-    regex: /✋👀/i,
+    regex: [/✋👀/i,/✋️👀/i,/✋🏻👀/i,/✋🏼👀/i,/✋🏽👀/i,/✋🏽👀/i,/✋🏾👀/i,/✋🏿👀/i],
     proccess: function (message, bot) {
       bot.sendMessage(message.chat.id, '✋👀');
     }
