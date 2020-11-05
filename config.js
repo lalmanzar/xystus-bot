dd0b246a4f6b8b7518bdbe4e0e8c4e72a5e5c605
Luis Almanzar <ruisu15@gmail.com>
2020-11-05 13:10:14 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,11 @@
         appUrl: process.env.APP_URL || ''
     };
 
+    var scrolller = {
+        apiUrl: process.env.SCROLLLER_API_URL || 'old.scrolller.com',
+        protocol: process.env.SCROLLLER_API_PROTOCOL || 'http',
+        path: process.env.SCROLLLER_API_PATH || '/api/gifs'
+    };
     module.exports = {
         telegram: telegram,
     };
